48716dab866a5619d0096684f16444220b4462eb
Luis Martinez <luis_mmartinez@live.com>
2021-07-29 12:06:08 -0400

    created and defined auth_controller for JWT authentication

---
+++ app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::AuthController < ApplicationController
+    skip_before_action :authorized, only: [:create]
+
+    # This controller action is used for user login
+    def create
+        # byebug
+        @user = User.find_by(email: user_login_params[:email])
+        if @user && @user.authenticate(user_login_params[:password])
+          token = encode_token({user_id: @user.id})
+          render json: { user: @user, jwt: token}, status: :accepted
+        else
+          render json: { message: 'Ivalid username or password'}, status: :unauthorized
+        end
+    end
+
+    private
+    def user_login_params
+      params.require(:user).permit(:email, :password)
+    end
+end
