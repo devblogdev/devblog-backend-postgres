fd0b66758ccc1ba70d86669c15b9b365582bcace
Luis Martinez <luis_mmartinez@live.com>
2021-08-14 18:12:29 -0400

    removed email_validatable file: removed uneeded comments from user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :posts
-    # validates :name, :last_name, presence: true, format: { with: /[^a-zA-Z -]/, on: :create}
     validates_presence_of :password, on: :create
     # Below line uses the 'valid_email2' gem
     validates :email, presence: true, 'valid_email_2/email': { mx: true, on: :create }
-    # Below line uses the 'email_validator' gem; not used in this app
-    # validates :email, email: {mode: :strict}
+    
 end
