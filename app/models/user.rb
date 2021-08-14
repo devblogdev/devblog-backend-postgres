a0c11cb3514bb0b468bd13d85dd5cb1d0073a750
Luis Martinez <luis_mmartinez@live.com>
2021-08-14 18:14:49 -0400

    removing email_validatable.rb file resolved the deployment error

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ApplicationRecord
     validates_presence_of :password, on: :create
     # Below line uses the 'valid_email2' gem
     validates :email, presence: true, 'valid_email_2/email': { mx: true, on: :create }
-    
 end
