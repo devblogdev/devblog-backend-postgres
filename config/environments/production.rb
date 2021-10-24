42b98e0a6c0c1eff88467a6cc4c793ae2f0ecfe7
Luis Martinez <luis_mmartinez@live.com>
2021-10-24 10:41:44 -0400

    add prerender token to use prerender gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,6 +90,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # Middleware for serving cached HTML with JavaScript to search engine robots 
+  config.middleware.use Rack::Prerender, prerender_token: ENV['PRERENDER_TOKEN']
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
