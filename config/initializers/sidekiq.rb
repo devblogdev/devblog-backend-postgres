708421973f70287c73eeded19e86b06de7706564
Luis Martinez <luis_mmartinez@live.com>
2021-12-31 20:11:22 -0500

    add code to initializers/sidekiq.rb for production

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,6 +2,7 @@
 #   {'name' => MyName, 'class' => MyJob, 'cron'  => '1 * * * *',  
 #   'queue' => default, 'active_job' => true }
 # ]
+
 # Sidekiq.configure_server do |config|
 #  config.redis = { host:'localhost', port: 6379, db: 1 }
 #  Sidekiq::Cron::Job.load_from_array! schedule
@@ -9,3 +10,16 @@
 # Sidekiq.configure_client do |config|
 #  config.redis = { host:'localhost', port: 6379, db: 1 }
 # end
+
+
+
+# NEWEST LINES
+Sidekiq.configure_server do |config|
+    # config.redis = { url: 'redis://redis.example.com:7372/0' }
+    config.redis = { url: ENV["REDIS_TLS_URL"] }
+end
+  
+Sidekiq.configure_client do |config|
+    # config.redis = { url: 'redis://redis.example.com:7372/0' }
+    config.redis = { url: ENV["REDIS_TLS_URL"] }
+end
