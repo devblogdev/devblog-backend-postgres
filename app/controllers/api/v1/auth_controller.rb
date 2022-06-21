7434507c6b37f81d1e6fbe23a65440a6e3b51d1d
Luis Martinez <luis_mmartinez@live.com>
2022-06-20 23:40:51 -0400

    update api/vi create action and sessions_controller omniauth_frontend action  to render private view for user

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -5,9 +5,14 @@ class Api::V1::AuthController < ApplicationController
     def create
       @user = User.find_by(email: user_login_params[:email].downcase)
       if @user && @user.authenticate(user_login_params[:password]) && @user.email_confirmed 
-        exp = Time.now.to_i + 24 * 3600
+        exp = Time.now.to_i + 3600
         token = encode_token({ user_id: @user.id, exp: exp })
-        render json: { user: @user, jwt: token, exp: exp }, status: :accepted
+        # render json: { user: @user, jwt: token, exp: exp }, status: :accepted
+        render json: { 
+          user: UserBlueprint.render(@user, view: :private),
+          jwt: token,
+          exp: exp
+        }
       else
         render json: { errors: ['Invalid email or password'] }, status: :unauthorized
       end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,11 @@ class SessionsController < ApplicationController
         user = User.from_omniauth(auth_frontend)
         if !user.provider.nil?
           token = encode_token({user_id: user.id})
-          render json: { user: user, jwt: token}, status: :accepted
+          # render json: { user: user, jwt: token}, status: :accepted
+          render json: { 
+            user: UserBlueprint.render(@user, view: :private),
+            jwt: token
+          }
         else
           render json: { message: ["An account has already been created with the email #{user.email}. Please login using your email."] }, status: :unauthorized
         end
