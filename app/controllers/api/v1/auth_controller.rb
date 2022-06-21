d734a40018e48bec19eb080d5500db893da6fd67
Luis Martinez <luis_mmartinez@live.com>
2022-06-20 23:41:19 -0400

    update api/vi create action and sessions_controller omniauth_frontend action  to render private view for user

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -3,16 +3,16 @@ class Api::V1::AuthController < ApplicationController
 
     # This controller action is used for noraml user login (not OAuth)
     def create
-      @user = User.find_by(email: user_login_params[:email].downcase)
-      if @user && @user.authenticate(user_login_params[:password]) && @user.email_confirmed 
-        exp = Time.now.to_i + 3600
-        token = encode_token({ user_id: @user.id, exp: exp })
+      user = User.find_by(email: user_login_params[:email].downcase)
+      if user && user.authenticate(user_login_params[:password]) && user.email_confirmed 
+        exp = Time.now.to_i + 24 * 3600
+        token = encode_token({ user_id: user.id, exp: exp })
         # render json: { user: @user, jwt: token, exp: exp }, status: :accepted
         render json: { 
-          user: UserBlueprint.render(@user, view: :private),
+          user: UserBlueprint.render(user, view: :private),
           jwt: token,
           exp: exp
-        }
+        }, status: :accepted
       else
         render json: { errors: ['Invalid email or password'] }, status: :unauthorized
       end
