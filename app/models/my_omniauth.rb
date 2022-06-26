01bfd7cf4dd148b0ff7c90ae1f791efe77daefbf
Luis Martinez <luis_mmartinez@live.com>
2022-06-26 00:53:42 -0400

    finilize initialization method in MyOmniauth module

--- app/models/my_omniauth.rb
+++ app/models/my_omniauth.rb
@@ -1,5 +1,12 @@
 module MyOmniauth
 
+    def self.config 
+        {
+            google: "google_oauth2",
+            twitter: "twitter"
+        }
+    end
+
     def self.google_oauth2_endpoints
         {
             tokens_endpoint: "https://oauth2.googleapis.com/token",
@@ -20,16 +27,25 @@ module MyOmniauth
                       :revoke_tokens_endpoint
 
         def initialize(provider)
-            self.provider = provider
+            MyOmniauth.config.each do |key, value|
+                key = key.to_s
+                if provider.include? key
+                    self.provider = value
+                    self.client_id = "#{ENV[(key.upcase + '_CLIENT_ID')]}"
+                    self.client_secret = "#{ENV[(key.upcase + '_CLIENT_SECRET')]}"
+                    break
+                end
+            end
             provider_endpoints = (provider + "_endpoints").to_sym
-            # byebug
-            # MyOmniauth.send(provider_endpoints).each do |key, value|
-            #     # self.send("#{key}", value)
-            #     puts "#{key} and value is #{value}"
-            # end
+            MyOmniauth.send(provider_endpoints).each do |key, value|
+                self.send("#{key}=", value)
+            end
         end
 
         def request_tokens(code)
+            self.class.post("https://oauth2.googleapis.com/token?#{@options[@provider.to_sym][:token_params]}", 
+            :headers => { "Content-type" => "application/x-www-form-urlencoded" }
+          )
         end
     
         def request_user_data(access_token)
@@ -41,15 +57,8 @@ module MyOmniauth
         def revoke_tokens
         end
 
-
-        protected
-        def credentials
-            {
-                client_id: "",
-                client_secret: ""
-            }
-        end
-
+        private
+        attr_accessor :client_id, :client_secret  
     end
 
 
